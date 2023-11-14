e5aaee59d9b7e37ceebeb46d59df266b1ff224e9
abhisinha <abhisheksinha@earthachakra.com>
2023-11-15 00:09:46 +0530

    build edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import IssueForm from "../../_components/IssueForm";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(params.id) },
+  });
+
+  if (!issue) return notFound();
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
