db59c0d393429ff68ff99ca8cf0b6f53d0074390
abhisinha <abhisheksinha@earthachakra.com>
2023-11-18 13:54:47 +0530

    improve loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,10 +7,10 @@ import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import SimpleMdeReact from "react-simplemde-editor";
 import { z } from "zod";
 
 type IssueFormData = z.infer<typeof issueSchema>;
@@ -49,10 +49,6 @@ const IssueForm = ({ issue }: Props) => {
     }
   });
 
-  const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-    ssr: false,
-  });
-
   return (
     <div className="max-w-xl ">
       {error && (
@@ -74,7 +70,7 @@ const IssueForm = ({ issue }: Props) => {
           control={control}
           defaultValue={issue?.description}
           render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+            <SimpleMdeReact placeholder="Description" {...field} />
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
