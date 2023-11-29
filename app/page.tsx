dfee59ed5170d706454201c767841684f54033a4
abhisinha <abhisheksinha@earthachakra.com>
2023-11-29 21:02:18 +0530

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
+import { useSearchParams } from "next/navigation";
 
-export default function Home() {
-  return <Pagination count={100} size={10} currentPage={10} />;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      count={100}
+      size={10}
+      currentPage={parseInt(searchParams.page) || 1}
+    />
+  );
 }
