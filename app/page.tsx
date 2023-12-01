01d581d3ea94d49785ed816b5f95e508df1c2519
abhisinha <abhisheksinha@earthachakra.com>
2023-12-01 19:35:12 +0530

    Layout dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,32 @@ import Image from "next/image";
 import Pagination from "./components/Pagination";
 import { useSearchParams } from "next/navigation";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
+import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
   return (
-    <LatestIssues />
-    // <Pagination
-    //   count={100}
-    //   size={10}
-    //   currentPage={parseInt(searchParams.page) || 1}
-    // />
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5" justify="between">
+        <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
   );
 }
